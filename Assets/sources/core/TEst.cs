340cf2db359a59b0ee90b30d9f59b3e652b86061
73023297+koufu193@users.noreply.github.com <toufu335@gmail.com>
2021-10-08 17:54:16 +0900

    a
    a

--- Assets/sources/core/TEst.cs
+++ Assets/sources/core/TEst.cs
@@ -7,7 +7,7 @@ public class TEst : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.write("a");
     }
 
     // Update is called once per frame
